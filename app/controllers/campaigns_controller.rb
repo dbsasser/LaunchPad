f6e24a03e825ce40c4811698349397b9db74d645
dbsasser <danielbsasser@gmail.com>
2020-05-09 23:33:45 -0400

    Added create method

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -4,4 +4,20 @@ class CampaignsController < ApplicationController
         @campaign = Campaign.new
     end 
 
+    def create
+        @campaign = current_user.created_campaigns.build(campaign_params)
+
+        if @campaign.save
+            redirect_to campaign_path(@campaign)
+        else
+            render 'new'
+        end
+    end
+
+    private 
+
+    def campaign_params
+        params.require(:campaign).permit(:title, :description, :goal, :picture)
+    end
+
 end
