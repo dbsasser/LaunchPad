1e5f1c26f511d9eb570f2631e7bbb25ed6cbb5b6
dbsasser <danielbsasser@gmail.com>
2020-05-10 17:15:26 -0400

    Installed Omniauth-facebook for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+
   has_many :created_campaigns, class_name: "Campaign", foreign_key: "creator_id"
   has_many :donations 
   has_many :campaigns, through: :donations
   has_many :comments
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end      
+  end
 end
