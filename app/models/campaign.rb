53f9d0c9d34e4d7be2c031eb04a4ff2a751fef82
dbsasser <danielbsasser@gmail.com>
2020-05-10 15:39:51 -0400

    Set up association to comments

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,4 +2,5 @@ class Campaign < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: "creator_id"
   has_many :donations
   has_many :users, through: :donations
+  has_many :comments
 end
