8935b23569ad4f83d31f5f03a59136051a51d90b
dbsasser <danielbsasser@gmail.com>
2020-05-11 00:14:08 -0400

    Added new and create actions

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,22 @@
 class DonationsController < ApplicationController
+
+    def new
+        @campaign = Campaign.find_by(id: params[:campaign_id])
+        @donation = @campaign.donations.build(user_id: current_user.id)
+      end
+    
+      def create
+        @donation = Donation.new(donation_params)
+
+        if @donation.save
+            redirect_to @donation.campaign
+        end
+      end
+    
+      private
+      
+      def donation_params
+        params.require(:donation).permit(:amount, :user_id, :campaign_id)
+      end
+
 end
