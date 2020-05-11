f76b41207f60cddbc1c3a8516521a01fcdff2d59
dbsasser <danielbsasser@gmail.com>
2020-05-10 20:06:12 -0400

    Updated from_omniauth to include user name and pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.name = auth.info.name
+      user.photo = auth.info.image
       user.password = Devise.friendly_token[0,20]
     end      
   end
