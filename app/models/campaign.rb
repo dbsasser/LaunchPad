25605986edbb387a731fcd6a75eab8c9a4e27bde
dbsasser <danielbsasser@gmail.com>
2020-05-11 22:44:04 -0400

    Made percent_raised method

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,4 +4,13 @@ class Campaign < ApplicationRecord
   has_many :users, through: :donations
   has_many :comments
   has_rich_text :description
+
+  def percent_raised
+    if self.goal > 0 && self.donations.sum(:amount) > 0
+      (self.donations.sum(:amount) * 100) / self.goal
+    else
+      0
+    end
+  end
+
 end
