a13021df3bd0035e382753e36ced0731b517a038
dbsasser <danielbsasser@gmail.com>
2020-05-14 20:56:50 -0400

    Redone give_donation method

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,14 +2,10 @@ class Donation < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
 
-  def give_donation(donation_params)
-    if self.user.funds >= donation_params[:donation][:amount]
-      self.user.funds -= donation_params[:donation][:amount]
-      @donation = Donation.new(donation_params)
-    end
-
-    if @donation.save
-      redirect_to @donation.campaign
+  def give_donation
+    if self.user.funds >= self.amount
+      self.user.funds -= self.amount
+      self.save
     end
   end 
 
