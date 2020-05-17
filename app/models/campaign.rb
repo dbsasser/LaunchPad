1921cc2d1004d586311b2858e58e209839305e8b
dbsasser <danielbsasser@gmail.com>
2020-05-17 10:49:22 -0400

    Added Validations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,6 +4,10 @@ class Campaign < ApplicationRecord
   has_many :users, through: :donations
   has_many :comments
   has_rich_text :description
+  validates :title, length: { in: 5..75} 
+  validates :picture, presence: true
+  validates :goal, numericality: { only_integer: true, greater_than: 0 }
+  validates :creator_id, presence: true
 
   def percent_raised
     if self.goal > 0 && self.donations.sum(:amount) > 0
