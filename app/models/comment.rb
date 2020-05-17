463e55ab1296ac13e4bf8b28ff4d35e8b45e328f
dbsasser <danielbsasser@gmail.com>
2020-05-17 10:52:10 -0400

    Added Validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :campaign_id, presence: true
 end
