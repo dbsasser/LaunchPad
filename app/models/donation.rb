d6c5ba2e90945f530d7eecdac2a56e3698895a33
dbsasser <danielbsasser@gmail.com>
2020-05-17 10:55:07 -0400

    Added Validations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,9 @@
 class Donation < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
+  validates :amount, numericality: { only_iteger: true, greater_than: 0}
+  validates :user_id, presence: true
+  validates :campaign_id, presence: true
 
   def give_donation
     if self.user.funds >= self.amount
