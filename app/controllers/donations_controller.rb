e816ab4f63698970e5dfb12e40f6549362dc930c
dbsasser <danielbsasser@gmail.com>
2020-05-18 23:41:42 -0400

    Created index action and view

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,20 +1,26 @@
 class DonationsController < ApplicationController
 
     def new
-        @campaign = Campaign.find_by(id: params[:campaign_id])
-        @donation = @campaign.donations.build(user_id: current_user.id)
-      end
+      @campaign = Campaign.find_by(id: params[:campaign_id])
+      @donation = @campaign.donations.build(user_id: current_user.id)
+    end
     
-      def create
-        @donation = Donation.new(donation_params)
-        @donation.give_donation && @donation.save
-        if @donation.give_donation
-          redirect_to campaign_path(@donation.campaign)
-        else
-          @msg = "Sorry, you don't have enough funds for that." 
-          render 'new'
-        end
+    def create
+      @donation = Donation.new(donation_params)
+      @donation.give_donation && @donation.save
+      if @donation.give_donation
+        redirect_to campaign_path(@donation.campaign)
+      else
+        @msg = "Sorry, you don't have enough funds for that." 
+        render 'new'
       end
+    end
+
+    def index
+      @campaign = Campaign.find_by(id: params[:campaign_id])
+    end
+
+
     
       private
       
