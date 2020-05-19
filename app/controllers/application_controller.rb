67c8ed3c56137f53b473eab72d0ec6626c8f97ef
dbsasser <danielbsasser@gmail.com>
2020-05-19 01:44:19 -0400

    Created before actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
+    
 
     def after_sign_in_path_for(resource)
         request.env['omniauth.origin'] || root_path
     end
+
+    private
+
+    def require_login 
+        redirect_to new_user_session_path unless user_signed_in?
+    end
+
+    def set_campaign
+        @campaign = Campaign.find_by(id: params[:id])
+    end
+
+    def authorize_campaign_creator
+        redirect_to 'campaigns#index' unless @campaign.creator == current_user
+    end
+
+
     
 end
